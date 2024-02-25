0d929ea868f09e46ef1c633440ea359daa24c772
Daniel Urra <danielurra75@gmail.com>
2024-02-25 05:53:21 -0500

    Uploading powershell script
    The main script that contains the code that will create the folders in a remote location (Network Drive).

---
+++ folder-creation.ps1
@@ -0,0 +1,20 @@
+﻿Clear
+
+$CSV = Import-Csv -Path "C:\Users\user\Documents\data.csv"
+
+
+$i = 0;
+
+ForEach ($Row in $CSV) {
+
+    $NAME_RAM = $($Row.'NOMBRE');
+
+    $FolderPath = "T:\_Amigos\$NAME_RAM";
+ 
+    #Create a New Folder
+    New-Item -ItemType Directory -Path $FolderPath
+        
+    $i += +1;
+    write-Host " $i Folders have been created.." -ForegroundColor Green -BackgroundColor DarkGreen
+    
+}
