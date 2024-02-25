1550be9c81c1793a868477c57eb9da5de1cadf4a
Daniel Urra <danielurra75@gmail.com>
2024-02-25 05:51:59 -0500

    Add files via upload

---
+++ set execution policy.ps1
@@ -0,0 +1,8 @@
+﻿#Change Directory
+cd ~
+#F8
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+# Network drive
+net use T: \\192.168.1.122\shared_folder
+# Test Local drive D: 
+New-Item -ItemType Directory -Path "D:\Test\99999-RICHARD\"
